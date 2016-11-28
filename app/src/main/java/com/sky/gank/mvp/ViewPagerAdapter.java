84670aac3ce88df159a8954d6c5842e620a58933
tonycheng93 <tonycheng93@outlook.com>
2016-11-28 21:18:21 +0800

    complete project

---
+++ app/src/main/java/com/sky/gank/mvp/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.sky.gank.mvp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by tonycheng on 2016/11/28.
+ */
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments;
+    private String[] mTitles;
+
+    public ViewPagerAdapter(FragmentManager fm, List<Fragment> fragments, String[] titles) {
+        super(fm);
+        mFragments = fragments;
+        mTitles = titles;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mTitles[position];
+    }
+}
